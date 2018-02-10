5b929ab263f8b580a2c4ed659c82f15a7b6f81df
Johan Martin <martin.johan@johan-martin.com>
2018-02-10 22:53:53 +0200

    added axios and update form

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,12 +1,34 @@
 import "@/assets/styles/styles.scss";
 import main from "@/app/main.html";
+import axios from "axios";
 
 console.log(__STATE__);
 
 class ViewController {
+  demo = "test";
+
   constructor() {
     const el = document.getElementById('root');
     el.outerHTML = main;
+    document.getElementById("js-coming-soon-form").addEventListener("submit", this.formSubmit);
+  }
+
+  formSubmit(e) {
+    e.preventDefault();
+    const keys = ["name", "email", "telephone", "status", "networking", "learning", "mentoring", "collaboration"];
+    const data = {}
+    const form = new FormData(e.target);
+    keys.forEach( (field) => data[field] = form.get(field));
+    console.log("data:", data);
+    axios({
+      data: data,
+      method: "post",
+      url: "http://localhost:5000/form"
+    }).then((response) => {
+      keys.map((key) => e.target[key].value = "");
+      document.getElementById("js-coming-soon-form").classList.add("hide");
+      document.getElementById("js-cn-sabbn-coming-soon-form-result").classList.remove("hide");
+    })
   }
 }
 
